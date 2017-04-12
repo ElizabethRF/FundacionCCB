3404fee9e57ccfc7b2e8d77a2b6a627e20af8bc2
ElizabethRF <eli_04nov@hotmail.com>
2017-04-12 14:58:05 -0500

    errores rió

--- Proyectos/app/models/user.rb
+++ Proyectos/app/models/user.rb
@@ -1,4 +1,6 @@
 class User <ActiveRecord::Base
+    has_many :projects
+    before_save {self.email =email.downcase}
     validates :nombre, presence: true,
                 length {minimum:3, maximum: 25}
     VALID_EMAIL_REGEX:/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
