b73be1f10f5ef4f002fdd7c433cc8b2b092b7dfa
ElizabethRF <eli_04nov@hotmail.com>
2017-04-13 07:18:01 -0500

    Registro
    esto ya no tiene errores

--- Proyectos/app/controllers/users_controller.rb
+++ Proyectos/app/controllers/users_controller.rb
@@ -6,8 +6,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            flash[:success] = "Bienvenido"
-            redirect_to articles_path
+            redirect_to projects_path
         else
             render 'new'
         end
@@ -15,6 +14,6 @@ class UsersController < ApplicationController
     
     private 
     def user_params
-         params.require(:user).permit(:nombre,:correo,:numero_telefono,:rol,:apellido_paterno,:apellido_materno,:nombre_de_usuario,:pasword_digest)
+         params.require(:user).permit(:nombre,:correo,:numero_telefono,:rol,:apellido_paterno,:apellido_materno,:nombre_de_usuario,:password_digest)
     end 
 end 
