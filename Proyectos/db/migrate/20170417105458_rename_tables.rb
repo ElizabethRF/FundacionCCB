f9378e2099c3b43abde76ac590768a6637bd9c7a
ElizabethRF <eli_04nov@hotmail.com>
2017-04-18 07:44:44 -0500

    Antes de que muriera pt2

--- Proyectos/db/migrate/20170417105458_rename_tables.rb
+++
@@ -1,9 +0,0 @@
-class RenameTables < ActiveRecord::Migration[5.0]
-  def change
-      rename_table :encuesta, :encuestas
-      rename_table :pregunta, :preguntas
-      rename_table :respuesta, :respuestas
-  end
-end
-
-
